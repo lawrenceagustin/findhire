a2894268df5aa40c8b7b0fe49c2c63589f6ef6be
lawrenceagustin <167160419+lawrenceagustin@users.noreply.github.com>
2024-12-10 17:08:34 +0800

    messages.php

--- messages.php
+++ messages.php
@@ -17,7 +17,7 @@ $messages = fetchMessages($user_id);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Messages</title>
-    <link rel="stylesheet" href="styles/messages.css"> <!-- Link to your CSS file -->
+    <link rel="stylesheet" href="styles/messages.css">
 </head>
 <body>
     <div class="container">
